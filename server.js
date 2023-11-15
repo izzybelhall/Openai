3e3df9f0c93a87e274b246321416b7b963e20312
Matt Hall <matt.hall236@icloud.com>
2023-11-14 23:51:17 -0500

    prettier formatting on html & js

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const openai = require('openai');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const openai = require("openai");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
 /*app.use(cors({
     origin: 'http://127.0.0.1:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));*/
-  
 
+const openaiApiKey = process.env.OPENAI_API_KEY;
 
-const openaiApiKey = process.env.OPENAI_API_KEY ; 
-  
-app.post('/api/get-reply', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-    res.setHeader('Vary', 'Origin');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-  try { 
+app.post("/api/get-reply", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  try {
     const { messages } = req.body;
 
-    const response = await new openai.Completion({ apiKey: openaiApiKey }).create({
-      model: 'gpt-3.5-turbo',
+    const response = await new openai.Completion({
+      apiKey: openaiApiKey,
+    }).create({
+      model: "gpt-3.5-turbo",
       messages,
     });
 
     const replyContent = response.choices[0].message.content;
     res.json({ replyContent });
   } catch (error) {
-    console.error('Error fetching reply:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching reply:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
